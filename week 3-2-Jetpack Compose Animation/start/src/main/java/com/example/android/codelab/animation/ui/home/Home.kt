06f10494f86d056ef799f4edc9e94bc5e8edfbb7
HyeonSeokGo <gohyeonseok02@gmail.com>
2021-11-21 20:50:35 +0900

    Todo 제거

--- week 3-2-Jetpack Compose Animation/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ week 3-2-Jetpack Compose Animation/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -179,7 +179,6 @@ fun Home() {
     val lazyListState = rememberLazyListState()
 
     // The background color. The value is changed by the current tab.
-    // TODO 1: Animate this color change.
     val backgroundColor by animateColorAsState(if (tabPage == TabPage.Home) Purple100 else Green300)
 
     // The coroutine scope for event handlers calling suspend functions.
@@ -292,7 +291,6 @@ private fun HomeFloatingActionButton(
                 contentDescription = null
             )
             // Toggle the visibility of the content with animation.
-            // TODO 2-1: Animate this visibility change.
             AnimatedVisibility(extended) {
                 Text(
                     text = stringResource(R.string.edit),
@@ -310,7 +308,6 @@ private fun HomeFloatingActionButton(
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun EditMessage(shown: Boolean) {
-    // TODO 2-2: The message should slide down from the top on appearance and slide up on
     //           disappearance.
     AnimatedVisibility(
         visible = shown,
@@ -390,7 +387,6 @@ private fun TopicRow(topic: String, expanded: Boolean, onClick: () -> Unit) {
         elevation = 2.dp,
         onClick = onClick
     ) {
-        // TODO 3: Animate the size change of the content.
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -475,7 +471,6 @@ private fun HomeTabIndicator(
     tabPositions: List<TabPosition>,
     tabPage: TabPage
 ) {
-    // TODO 4: Animate these value changes.
     val transition = updateTransition(
         tabPage,
         label = "Tab indicator"
@@ -593,7 +588,6 @@ private fun WeatherRow(
  */
 @Composable
 private fun LoadingRow() {
-    // TODO 5: Animate this value between 0f and 1f, then back to 0f repeatedly.
     val infiniteTransition = rememberInfiniteTransition()
     val alpha by infiniteTransition.animateFloat(
         initialValue = 0f,
@@ -668,7 +662,6 @@ private fun TaskRow(task: String, onRemove: () -> Unit) {
 private fun Modifier.swipeToDismiss(
     onDismissed: () -> Unit
 ): Modifier = composed {
-    // TODO 6-1: Create an Animatable instance for the offset of the swiped element.
     val offsetX = remember { Animatable(0f) }
     pointerInput(Unit) {
         // Used to calculate a settling position of a fling animation.
@@ -678,14 +671,12 @@ private fun Modifier.swipeToDismiss(
             while (true) {
                 // Wait for a touch down event.
                 val pointerId = awaitPointerEventScope { awaitFirstDown().id }
-                // TODO 6-2: Touch detected; the animation should be stopped.
                 // Prepare for drag events and record velocity of a fling.
                 offsetX.stop()
                 val velocityTracker = VelocityTracker()
                 // Wait for drag events.
                 awaitPointerEventScope {
                     horizontalDrag(pointerId) { change ->
-                        // TODO 6-3: Apply the drag change to the Animatable offset.
                         // Record the velocity of the drag.
                         val horizontalDragOffset = offsetX.value + change.positionChange().x
                         launch {
@@ -703,13 +694,8 @@ private fun Modifier.swipeToDismiss(
                     lowerBound = -size.width.toFloat(),
                     upperBound = size.width.toFloat()
                 )
-                // TODO 6-4: Calculate the eventual position where the fling should settle
-                //           based on the current offset value and velocity
-                // TODO 6-5: Set the upper and lower bounds so that the animation stops when it
-                //           reaches the edge.
+
                 launch {
-                    // TODO 6-6: Slide back the element if the settling position does not go beyond
-                    //           the size of the element. Remove the element if it does.
                     if (targetOffsetX.absoluteValue <= size.width) {
                         offsetX.animateTo(targetValue = 0f, initialVelocity = velocity)
                     } else {
@@ -719,9 +705,7 @@ private fun Modifier.swipeToDismiss(
                 }
             }
         }
-    }
-        // TODO 6-7: Use the animating offset value here.
-        .offset { IntOffset(offsetX.value.roundToInt(), 0) }
+    }.offset { IntOffset(offsetX.value.roundToInt(), 0) }
 }
 
 @Preview
